d5dd45d8f0aebdd944b28d3421010cab8a74dddb
Vlad <b4.v061@gmail.com>
2024-02-12 16:33:56 +0200

    final version

---
+++ script.js
@@ -0,0 +1,35 @@
+//Task number 1
+let variable = "ami";
+variable = "bruh"/2+variable;
+console.log(variable);
+
+//Task number 2
+let userVlad = {
+    userName: "Vladyslav",
+    userNickname: "b4v06",
+    userPhone: "123456789",
+    userMail: "bruh@gmail.com",
+    userSex: "male",
+    userHobby: "chess",
+    userHasPet: true,
+};
+let userOlga = {
+    userName: "Olga",
+    userNickname: "OlgaM",
+    userPhone: "456123789",
+    userMail: "olga.m@gmail.com",
+    userSex: "female",
+    userHobby: "movies",
+    userHasPet: true,
+};
+let userIhor = {
+    userName: "Ihor",
+    userNickname: "JosephBiden",
+    userPhone: "789123456",
+    userMail: "joseph.biden@gmail.com",
+    userSex: "male",
+    userHobby: "computer games",
+    userHasPet: false,
+};
+const users = [userVlad, userOlga, userIhor];
+console.log(users);
